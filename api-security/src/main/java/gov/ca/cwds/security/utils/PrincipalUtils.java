38c3c5860371ce6f9da2bba6302be394ab8032f9
David R. Smith <David.Smith@osi.ca.gov>
2018-08-16 17:19:40 -0700

    Perry token check

--- api-security/src/main/java/gov/ca/cwds/security/utils/PrincipalUtils.java
+++ api-security/src/main/java/gov/ca/cwds/security/utils/PrincipalUtils.java
@@ -1,36 +1,40 @@
 package gov.ca.cwds.security.utils;
 
-import gov.ca.cwds.security.realm.PerryAccount;
+import java.util.Optional;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 
-import java.util.Optional;
+import gov.ca.cwds.security.realm.PerryAccount;
 
 /**
  * Created by Alexander Serbin on 12/21/2017.
  */
 public final class PrincipalUtils {
 
-    public static final String DEFAULT_USER_ID = "0X5";
+  public static final String DEFAULT_USER_ID = "0X5";
 
-    private PrincipalUtils() {}
+  private PrincipalUtils() {}
 
-    public static PerryAccount getPrincipal() {
-        Optional<PerryAccount> perryAccount = Optional.empty();
-        Subject currentUser = SecurityUtils.getSubject();
-        if (currentUser.getPrincipals() != null) {
-            perryAccount = currentUser.getPrincipals().asList().stream().filter(principal -> principal instanceof PerryAccount).findAny();
-        }
-        return perryAccount.orElseThrow(IllegalStateException::new);
+  public static PerryAccount getPrincipal() {
+    Optional<PerryAccount> perryAccount = Optional.empty();
+    final Subject currentUser = SecurityUtils.getSubject();
+
+    if (currentUser.getPrincipals() != null) {
+      perryAccount = currentUser.getPrincipals().asList().stream()
+          .filter(principal -> principal instanceof PerryAccount).findAny();
     }
 
-    public static String getStaffPersonId() {
-        PerryAccount perryAccount = getPrincipal();
-        if (StringUtils.isEmpty(perryAccount.getStaffId())) {
-            return DEFAULT_USER_ID;
-        }
-        return perryAccount.getStaffId();
+    return perryAccount.orElseThrow(IllegalStateException::new);
+  }
+
+  public static String getStaffPersonId() {
+    final PerryAccount perryAccount = getPrincipal();
+    if (StringUtils.isEmpty(perryAccount.getStaffId())) {
+      return DEFAULT_USER_ID;
     }
+    return perryAccount.getStaffId();
+  }
 
 }
