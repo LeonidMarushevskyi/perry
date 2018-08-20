19fda396963edf20f2bc01b48ed17c019f3130e8
David R. Smith <David.Smith@osi.ca.gov>
2018-08-20 09:16:06 -0700

    Perry token check

--- src/main/java/gov/ca/cwds/healthcheck/CognitoHealthContributor.java
+++ src/main/java/gov/ca/cwds/healthcheck/CognitoHealthContributor.java
@@ -1,7 +1,5 @@
 package gov.ca.cwds.healthcheck;
 
-import com.amazonaws.services.cognitoidp.model.AWSCognitoIdentityProviderException;
-import gov.ca.cwds.idm.service.cognito.CognitoServiceFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
@@ -9,11 +7,16 @@ import org.springframework.boot.actuate.health.Status;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.cognitoidp.model.AWSCognitoIdentityProviderException;
+
+import gov.ca.cwds.idm.service.cognito.CognitoServiceFacade;
+
 @Profile("idm")
 @Component(value = "cognitoHealthCheck")
 public class CognitoHealthContributor implements HealthIndicator {
 
-  @Autowired private CognitoServiceFacade cognitoServiceFacade;
+  @Autowired
+  private CognitoServiceFacade cognitoServiceFacade;
 
   @Override
   public Health health() {
@@ -24,4 +27,5 @@ public class CognitoHealthContributor implements HealthIndicator {
       return Health.down().withException(awsException).build();
     }
   }
+
 }
